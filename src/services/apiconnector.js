cdf564e51197ef6634b21a28c9a51f17c7914f99
Kushagra-richhariya <kushagrarichhariya713@gmail.com>
2025-07-16 00:40:44 +0530

    bug

--- src/services/apiconnector.js
+++ src/services/apiconnector.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const axiosInstance = axios.create({
-    baseURL:  process.env.REACT_APP_API_URL,
+    baseURL:  "https://vidyarthi-sxww.onrender.com",
     withCredentials: true,
 });
 
